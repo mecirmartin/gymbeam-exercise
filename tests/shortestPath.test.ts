accdaefc6d6dc3ef407ba2839318777ff5505c88
mecirmartin <mecir.martin@gmail.com>
2023-09-12 16:54:37 +0200

    test: test shortest-path endpoint

---
+++ tests/shortestPath.test.ts
@@ -0,0 +1,43 @@
+import request from 'supertest'
+
+import app, { server } from '../src/index'
+
+describe('/shortest-path endpoint', () => {
+  it('Should return 400 if invalid request body is specified', async () => {
+    const res = await request(app)
+      .get('/shortest-path')
+      .send({
+        startingPosition: {
+          x: 0,
+          y: 0,
+          z: 0,
+        },
+      })
+    expect(res.statusCode).toEqual(400)
+  })
+  it('Should return 400 if invalid request body is specified', async () => {
+    const res = await request(app)
+      .get('/shortest-path')
+      .send({
+        products: ['product-1', 'product-2'],
+      })
+    expect(res.statusCode).toEqual(400)
+  })
+  it('Should return 200 if valid request body is specified', async () => {
+    const res = await request(app)
+      .get('/shortest-path')
+      .send({
+        products: ['product-1', 'product-2'],
+        startingPosition: {
+          x: 0,
+          y: 0,
+          z: 0,
+        },
+      })
+    expect(res.statusCode).toEqual(200)
+  })
+})
+
+afterAll(() => {
+  server.close()
+})
